68694f54547eab853a8d48a84599eb2d0049cf1d
blizzardKv <makemewanna@yandex.ru>
2020-09-13 19:15:42 +0300

    init repo

---
+++ src/components/Server/Server.vue
@@ -0,0 +1,24 @@
+<template>
+    <li class="list-group-item"
+        style="cursor: pointer;"
+        @click="serverSelected">
+      Server #{{ server.id }}
+    </li>
+</template>
+
+<script>
+  import { eventBus } from "../../main";
+
+  export default {
+        props: ['server'],
+        methods: {
+            serverSelected() {
+                eventBus.$emit('serverSelected', this.server);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Server/Servers.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <Server v-for="server in serversData" :server="server" />
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Server from "./Server.vue";
+    export default {
+        components: {
+            Server
+        },
+        data() {
+            return {
+                serversData: [
+                    { id: 1, status: 'Normal'},
+                    { id: 2, status: 'Crispy'},
+                    { id: 3, status: 'Danger'},
+                    { id: 4, status: 'Normal'},
+                ]
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
